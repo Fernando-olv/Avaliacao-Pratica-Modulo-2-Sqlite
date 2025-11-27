b7da6162ae5d267cfcbac4a544f44a09f2477230
faverim <gfaverim@gmail.com>
2025-11-27 14:57:07 +0000

    Gabriel Faverim dos Santos Modulo 2 Manhã

--- app_sqlite.py
+++ app_sqlite.py
@@ -12,3 +12,42 @@ O que o script deve fazer:
 7) Deletar registros
 
 """
+import sqlite3
+
+conexao = sqlite3.connect("escola.db")
+
+conexao.execute("""
+CREATE TABLE IF NOT EXISTS alunos(
+id INTEGER NOT NULL PRIMARY KEY,
+nome TEXT not NULL,
+idade INTEGER,
+email TEXT)
+
+""")
+
+conexao.execute("""
+INSERT or IGNORE INTO alunos (nome, idade, email) VALUES
+('gabriel', 22, "faverim@gmail"),
+("nandinho", 40, "nandinho@gmail"),
+("nega", 38, "nega@gmail");
+
+ """)
+
+#Exibir todos os alunos 
+lista_alunos = conexao.execute("SELECT * FROM alunos").fetchall()
+print("alunos:", lista_alunos)
+
+#Exibir o nome do aluno com 
+aluno = conexao.execute("SELECT * FROM alunos WHERE nome = 'gabriel' ").fetchall()
+print("aluno gabriel:", aluno)
+
+#atualizar algum registro e mostrar ele atualizado
+aluno = conexao.execute("UPDATE alunos SET nome = 'faverim' WHERE id = 1;").fetchall()
+print("aluno",aluno)
+
+conexao.execute("DELETE FROM alunos WHERE id = 2")
+
+
+
+
+
