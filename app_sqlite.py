9f59aa2bdda898dcb812a519e13e4c06c90455bd
mariaeduardaalbm23-ship-it <mariaeduardaalbm.23@gmail.com>
2025-11-27 19:49:10 +0000

    Maria Eduarda Almeida Bomfim Modulo 2  tarde

--- app_sqlite.py
+++ app_sqlite.py
@@ -12,3 +12,36 @@ O que o script deve fazer:
 7) Deletar registros
 
 """
+
+import sqlite3
+
+conexao = sqlite3.connect("escola.db")
+
+conexao.execute("""
+CREATE TABLE IF NOT EXISTS alunos(
+id INTEGER NOT NULL PRIMARY KEY,
+nome TEXT NOT NULL,
+idade INTEGER,
+email TEXT);   
+
+""")
+
+conexao.execute("""
+INSERT INTO alunos (nome,idade,email) VALUES 
+                ("eduarda",16, "eduarda@gmail.com" ),
+                ("victor", 16, "victorhugo@gmail.com"),
+                ("gabriel", 18, "gabrielopes@gmail.com"),
+                ("sarita" , 17, "sarita@gmail.com")
+""")
+lista_alunos = conexao.execute ("SELECT * FROM alunos").fetchall()
+print ("alunos:", lista_alunos)
+
+aluno = conexao.execute("SELECT * FROM alunos WHERE id = 3").fetchone()
+print("Aluno:", aluno)
+
+conexao.execute ("UPDATE alunos SET nome = 'victor' WHERE nome = 'gabriel' ")
+
+listar_alunos = conexao.execute ("SELECT * FROM alunos "). fetchall ()
+print ("alunos:" , lista_alunos)
+
+conexao.execute("DELETE FROM alunos WHERE id=3")
