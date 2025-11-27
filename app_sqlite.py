0cb96d7602a2b57fa2ff392a601b0ce2c8fa65f7
ArroZh <hannah.playg@gmail.com>
2025-11-27 19:47:16 +0000

    Hannah Yasmin Rodrigues de Lima  Módulo 2  Tarde

--- app_sqlite.py
+++ app_sqlite.py
@@ -12,3 +12,39 @@ O que o script deve fazer:
 7) Deletar registros
 
 """
+import sqlite3
+
+conexao = sqlite3.connect("escola.db")
+
+conexao.execute(""" 
+CREATE TABLE IF NOT EXISTS Alunos ( 
+id INTEGER NOT NULL PRIMARY KEY , 
+Nome TEXT NOT NULL,	
+Idade INTEGER ,
+Email TEXT );
+""")
+
+conexao.execute("""
+INSERT INTO Alunos (Nome,Idade,Email)VALUES 
+                ("Diego", 15, "pequena_camponesa@gmail.com"), 
+                ("Guilherme", 16, "pai_de_familia@gmail.com"),
+                ("Samuel", 16, "nike@gmail.com");
+
+""")
+
+lista_alunos = conexao.execute("SELECT * FROM alunos").fetchall()
+print("Alunos:",lista_alunos)
+
+Aluno = conexao.execute("SELECT * FROM Alunos WHERE id =2").fetchone()
+print("Aluno 3", Aluno)
+
+#atualizar registros
+conexao.execute("UPDATE Alunos SET nome = 'Camponesa' WHERE Nome ='Diego'")
+
+lista_alunos = conexao.execute("SELECT * FROM alunos").fetchall()
+print("Alunos:",lista_alunos)
+
+#Deletar registros
+conexao.execute("DELETE FROM Alunos WHERE nome = 'Samuel'")
+lista_alunos = conexao.execute("SELECT * FROM alunos").fetchall()
+print("Alunos:",lista_alunos)
