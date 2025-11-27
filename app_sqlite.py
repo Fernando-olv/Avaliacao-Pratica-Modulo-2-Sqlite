e5ebbb5fac8b2c557c847e37a5f83111a00f5b15
Sazz-75 <samuelvictoralves0@gmail.com>
2025-11-27 19:49:54 +0000

    Samuel victor alves dias modulo 2 tarde

--- app_sqlite.py
+++ app_sqlite.py
@@ -12,3 +12,59 @@ O que o script deve fazer:
 7) Deletar registros
 
 """
+
+import sqlite3
+
+conexao = sqlite3.connect("escola.db")
+
+
+conexao.execute("""
+CREATE TABLE IF NOT EXISTS alunos (
+id INTEGER NOT NULL PRIMARY KEY,
+nome TEXT NOT NULL,
+idade INTEGER, 
+email TEXT);
+""")
+conexao.execute(""" INSERT INTO Alunos (nome, idade,email) VALUES
+                ("Samuel",12,"samuelvictor@gmail"),
+                ("Pablo maia", 289,"PAblo777@gmail.com"),
+                ("Paolo guerra", 11289,"luizkjkkkkk@gmail.com") 
+
+
+""")
+#listar todos 
+
+lista_alunos = conexao.execute("SELECT * FROM alunos").fetchall()
+print("alunos", lista_alunos)
+
+#buscar
+
+aluno = conexao.execute("SELECT * FROM alunos WHERE id = 3").fetchone()
+print("alunos 3", aluno)
+
+#atualizar
+
+conexao.execute("UPDATE alunos SET nome = 'matteo' WHERE nome = 'Lana'")
+lista_alunos = conexao.execute("SELECT * FROM alunos").fetchall()
+print("Alunos:",lista_alunos)
+
+#deletar
+conexao.execute("DELETE FROM Alunos WHERE nome = 'Samuel' ")
+lista_alunos = conexao.execute("SELECT * FROM alunos").fetchall()
+print("alunos:", lista_alunos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
